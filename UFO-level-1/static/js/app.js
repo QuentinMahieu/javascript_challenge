a4f969c333c7ba2f2de83bdbb0920c68977ac792
QuentinMahieu <quentinmahieu@Mac-Quentin.local>
2020-11-30 14:34:20 +1100

    final part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,16 +4,19 @@ var tableData = data;
 // Select the table body
 var tbody = d3.select('tbody');
 // create the table with each information
-tableData.forEach((ufo) => {
-  var row = tbody.append("tr");
-  Object.entries(ufo).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
+function maketable(data){
+  tbody.selectAll('tr').remove();
+  data.forEach((x) => {
+    var row = tbody.append("tr");
+    Object.entries(x).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
-});
-// Select the button
+};
+
+// Select the button and form
 var button = d3.select("#filter-btn");
-// Select the form
 var form = d3.select("#form-date");
 // Create event handlers 
 button.on("click", runEnter);
@@ -22,41 +25,23 @@ form.on("submit",runEnter);
 function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
+  filteredData = tableData;
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-  if (inputValue.length !== 0 && inputValue !== null ){
-    // filter the data
+  var date = d3.select("#datetime").node().value;
+  // if date not found
+  if (date.length !== 0 && date !== null ){
     dates = []
-    tableData.map(ufo => {
-      var date = ufo.datetime; 
-      dates.push(date);
+    tableData.map(x => {
+      var d = x.datetime; 
+      dates.push(d);
     });
-    if (dates.includes(inputValue) === false){
+    if (dates.includes(date) === false){
       alert("No data found for input date");
     }else{
-      tbody.selectAll('tr').remove();
-      var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-      //creates the table with the filtered data
-      filteredData.forEach((ufo) => {
-        var row = tbody.append("tr");
-        Object.entries(ufo).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-      });
+      var filteredData = tableData.filter(x => x.datetime === date);
     };  
-  } 
-  else {
-    tbody.selectAll('tr').remove();
-    tableData.forEach((ufo) => {
-      var row = tbody.append("tr");
-      Object.entries(ufo).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    });
   }
-}
-  
+  maketable(filteredData);
+};
+//makes the table at the opening of the page
+maketable(tableData);
